298745a59f19bb58c5ff2f2c209ba3d93152ddc2
w1lqa <mrsocrushitel46@gmail.com>
2025-04-12 15:56:45 +0300

    hm1

---
+++ hw1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2.1.Домашняя работа: Hello, World!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img class="logo" src="https://mospolytech.ru/local/templates/main/dist/img/logos/mospolytech-logo-white.svg" alt="">
+        <h1>2.1.Домашняя работа: Hello, World!</h1>
+    </header>
+
+    <main>
+        <div class="greeting">
+            <?php 
+                $hour = date('H');
+                $greeting = ($hour < 12) ? 'Доброе утро' : (($hour < 18) ? 'Добрый день' : 'Добрый вечер');
+                echo "<h2>$greeting, Мир!</h2>";
+            ?>
+        </div>
+    </main>
+
+    <footer>
+        Задание для самостоятельной работы «Hello, World!»
+    </footer>
+</body>
+</html>
